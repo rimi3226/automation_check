f8c6d5807836fe2adfe46215362dfedda82ec160
Rimi <lora3226@inu.ac.kr>
2023-04-28 17:14:51 +0900

    add twilio

--- controllers/pnum.js
+++ controllers/pnum.js
@@ -1,24 +1,33 @@
-const html = `
-<!DOCTYPE html>
-<html>
-    <script>
-        alert('인증번호가 문자로 발송되었습니다.');
-    </script>
-</html>
-`;
+//dotenv 불러오기
+const dotenv = require('dotenv');
+dotenv.config();
+
+//twilio 불러오기
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+
 
 // 인증번호 생성 & return
 exports.pnum = async (req, res) => {
     //난수 생성
-    const num=Math.floor(Math.random() * (100000 - 10000) + 10000);
+    const num = Math.floor(Math.random() * (100000 - 10000) + 10000);
     console.log(num);
-    req.session.auth_num=num;
-    
-    /*
+    req.session.auth_num = num;
 
-    twillio로 보내는 코드
-
-    */
+    //twilio
+    try {
+        client.messages
+            .create({
+                body: 'STEP-UP 출석 확인 인증 번호입니다 : '+num,
+                from: '+16203494088',
+                to: '+821095036290'
+            })
+            .then(message => console.log(message.sid));
+    } catch (err) {
+        console.log(err);
+    }
 
 
     res.redirect(`/?Auth=complete`);
